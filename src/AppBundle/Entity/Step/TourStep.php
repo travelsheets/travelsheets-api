86975c88d1d7294e06f39e4eaa36084f8b88cfc5
Quentin Machard <quentin.machard@gmail.com>
2017-08-09 12:12:00 +0200

    Create Tour Step

---
+++ src/AppBundle/Entity/Step/TourStep.php
@@ -0,0 +1,75 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: quentinmachard
+ * Date: 09/08/2017
+ * Time: 12:10
+ */
+
+namespace AppBundle\Entity\Step;
+
+
+use AppBundle\Entity\AbstractStep;
+use AppBundle\Entity\Place;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Class TourStep
+ * @package AppBundle\Entity\Step
+ *
+ * @ORM\Entity(repositoryClass="AppBundle\Repository\StepRepository")
+ * @ORM\Table(name="tour_step")
+ *
+ */
+class TourStep extends AbstractStep
+{
+    /**
+     * @var Place
+     * @ORM\ManyToOne(targetEntity="AppBundle\Entity\Place", inversedBy="id")
+     * @ORM\JoinColumn(nullable=true)
+     */
+    private $place;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="type", type="string", length=255)
+     */
+    private $type;
+
+    /**
+     * @return Place
+     */
+    public function getPlace()
+    {
+        return $this->place;
+    }
+
+    /**
+     * @param Place $place
+     * @return $this
+     */
+    public function setPlace(Place $place = null)
+    {
+        $this->place = $place;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getType()
+    {
+        return $this->type;
+    }
+
+    /**
+     * @param string $type
+     * @return $this
+     */
+    public function setType($type)
+    {
+        $this->type = $type;
+        return $this;
+    }
+}
