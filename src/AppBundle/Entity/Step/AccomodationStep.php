36b96beb52a31b30d3bde8ad82b00d00f50c851a
Quentin Machard <quentin.machard@gmail.com>
2017-12-04 14:41:36 +0100

    Add new fields to AccomodationStep

--- src/AppBundle/Entity/Step/AccomodationStep.php
+++ src/AppBundle/Entity/Step/AccomodationStep.php
@@ -11,6 +11,7 @@ namespace AppBundle\Entity\Step;
 
 use AppBundle\Entity\AbstractStep;
 use AppBundle\Entity\Place;
+use DateTime;
 use Doctrine\ORM\Mapping as ORM;
 
 /**
@@ -36,6 +37,41 @@ class AccomodationStep extends AbstractStep
      * @ORM\JoinColumn(nullable=true)
      */
     private $place;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="company", type="string", length=255, nullable=true)
+     */
+    private $company;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="booking_number", type="string", length=255, nullable=true)
+     */
+    private $bookingNumber;
+
+    /**
+     * @var DateTime
+     *
+     * @ORM\Column(name="opening_luggage", type="datetime", nullable=true)
+     */
+    private $openingLuggage;
+
+    /**
+     * @var DateTime
+     *
+     * @ORM\Column(name="closing_luggage", type="datetime", nullable=true)
+     */
+    private $closingLuggage;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="seat", type="string", nullable=true)
+     */
+    private $seat;
 
     /**
      * @var string
@@ -91,6 +127,95 @@ class AccomodationStep extends AbstractStep
         return $this;
     }
 
+    /**
+     * @return string
+     */
+    public function getCompany()
+    {
+        return $this->company;
+    }
+
+    /**
+     * @param string $company
+     * @return AccomodationStep
+     */
+    public function setCompany($company)
+    {
+        $this->company = $company;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getBookingNumber()
+    {
+        return $this->bookingNumber;
+    }
+
+    /**
+     * @param string $bookingNumber
+     * @return AccomodationStep
+     */
+    public function setBookingNumber($bookingNumber)
+    {
+        $this->bookingNumber = $bookingNumber;
+        return $this;
+    }
+
+    /**
+     * @return DateTime
+     */
+    public function getOpeningLuggage()
+    {
+        return $this->openingLuggage;
+    }
+
+    /**
+     * @param DateTime $openingLuggage
+     * @return AccomodationStep
+     */
+    public function setOpeningLuggage($openingLuggage)
+    {
+        $this->openingLuggage = $openingLuggage;
+        return $this;
+    }
+
+    /**
+     * @return DateTime
+     */
+    public function getClosingLuggage()
+    {
+        return $this->closingLuggage;
+    }
+
+    /**
+     * @param DateTime $closingLuggage
+     * @return AccomodationStep
+     */
+    public function setClosingLuggage($closingLuggage)
+    {
+        $this->closingLuggage = $closingLuggage;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getSeat()
+    {
+        return $this->seat;
+    }
+
+    /**
+     * @param string $seat
+     * @return AccomodationStep
+     */
+    public function setSeat($seat)
+    {
+        $this->seat = $seat;
+        return $this;
+    }
 
     /**
      * @return string
