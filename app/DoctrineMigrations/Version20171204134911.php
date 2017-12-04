97ded6d51333faa03c9033256a9db904b7c5b091
Quentin Machard <quentin.machard@gmail.com>
2017-12-04 14:49:43 +0100

    Remove luggages fields

---
+++ app/DoctrineMigrations/Version20171204134911.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Application\Migrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+class Version20171204134911 extends AbstractMigration
+{
+    /**
+     * @param Schema $schema
+     */
+    public function up(Schema $schema)
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE accomodation_step DROP opening_luggage, DROP closing_luggage, DROP seat');
+    }
+
+    /**
+     * @param Schema $schema
+     */
+    public function down(Schema $schema)
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE accomodation_step ADD opening_luggage DATETIME DEFAULT NULL, ADD closing_luggage DATETIME DEFAULT NULL, ADD seat VARCHAR(255) DEFAULT NULL COLLATE utf8_unicode_ci');
+    }
+}
--- src/AppBundle/Entity/Step/AccomodationStep.php
+++ src/AppBundle/Entity/Step/AccomodationStep.php
@@ -53,27 +53,6 @@ class AccomodationStep extends AbstractStep
     private $bookingNumber;
 
     /**
-     * @var DateTime
-     *
-     * @ORM\Column(name="opening_luggage", type="datetime", nullable=true)
-     */
-    private $openingLuggage;
-
-    /**
-     * @var DateTime
-     *
-     * @ORM\Column(name="closing_luggage", type="datetime", nullable=true)
-     */
-    private $closingLuggage;
-
-    /**
-     * @var string
-     *
-     * @ORM\Column(name="seat", type="string", nullable=true)
-     */
-    private $seat;
-
-    /**
      * @var string
      *
      * @ORM\Column(name="type", type="string", length=255)
@@ -160,60 +139,6 @@ class AccomodationStep extends AbstractStep
     public function setBookingNumber($bookingNumber)
     {
         $this->bookingNumber = $bookingNumber;
-        return $this;
-    }
-
-    /**
-     * @return DateTime
-     */
-    public function getOpeningLuggage()
-    {
-        return $this->openingLuggage;
-    }
-
-    /**
-     * @param DateTime $openingLuggage
-     * @return AccomodationStep
-     */
-    public function setOpeningLuggage($openingLuggage)
-    {
-        $this->openingLuggage = $openingLuggage;
-        return $this;
-    }
-
-    /**
-     * @return DateTime
-     */
-    public function getClosingLuggage()
-    {
-        return $this->closingLuggage;
-    }
-
-    /**
-     * @param DateTime $closingLuggage
-     * @return AccomodationStep
-     */
-    public function setClosingLuggage($closingLuggage)
-    {
-        $this->closingLuggage = $closingLuggage;
-        return $this;
-    }
-
-    /**
-     * @return string
-     */
-    public function getSeat()
-    {
-        return $this->seat;
-    }
-
-    /**
-     * @param string $seat
-     * @return AccomodationStep
-     */
-    public function setSeat($seat)
-    {
-        $this->seat = $seat;
         return $this;
     }
 
