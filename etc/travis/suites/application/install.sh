e3b441762b5c1c2975048cd15549ba9daa036538
Quentin Machard <quentin.machard@gmail.com>
2018-03-21 20:03:20 +0100

    Initialize Travis (composer install and check)

---
+++ etc/travis/suites/application/install.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+source "$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)/../../../bash/common.lib.sh"
+source "$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)/../../../bash/application.sh"
+
+print_header "Installing dependencies" "Travelsheets"
+run_command "composer install --no-interaction --prefer-dist" || exit $?
+
+#print_header "Warming up dependencies" "Travelsheets"
+#run_command "yarn install" || exit $?
