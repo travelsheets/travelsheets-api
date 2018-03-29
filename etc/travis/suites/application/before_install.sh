f6062b69fb6af1eac193c176247fa98e3f327b80
Quentin Machard <quentin.machard@gmail.com>
2018-03-29 20:54:39 +0200

    Add fixture testing in Travis

--- etc/travis/suites/application/before_install.sh
+++ etc/travis/suites/application/before_install.sh
@@ -3,5 +3,5 @@
 source "$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)/../../../bash/common.lib.sh"
 source "$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)/../../../bash/application.sh"
 
-print_header "Activating memcached extension" "Travelsheets"
+print_header "Activating memcached extension" "TravelSheets"
 run_command "echo \"extension = memcached.so\" >> ~/.phpenv/versions/$(phpenv version-name)/etc/conf.d/travis.ini" || exit $?
--- etc/travis/suites/application/script.sh
+++ etc/travis/suites/application/script.sh
@@ -6,14 +6,10 @@ code=0
 commands=(
     validate-composer
     validate-composer-security
-#    validate-behat-features
     validate-doctrine-schema
-#    validate-twig
+    test-fixtures
 #    test-phpspec
 #    test-phpunit
-#    test-fixtures
-#    test-behat-without-javascript
-#    test-old-behat-without-javascript
 )
 
 for command in ${commands[@]}; do
